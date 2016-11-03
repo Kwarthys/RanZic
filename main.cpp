3ed8d9a0723a1af7f6be798310bb529e1dc1f609
Kwarthys <Kwarthys@gmail.com>
2016-11-03 21:38:22 +0100

    v0.0.1 XMLElements
    Now with the test in main (still learning git bash)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+#include "XMLElement.h"
+
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+
+    std::string name = "premier", name2 = "deux";
+    XMLElement e = XMLElement(name);
+    XMLElement e2 = XMLElement(name2);
+
+    std::string value = "Je suis le premier", value2 = "value2";
+    e.setValue(value);e2.setValue(value2);
+    std::string  attr = "Premier=\"1\"", at2 = "id=\"id2\"";
+    e.addAttribute(attr); e2.addAttribute(at2);
+
+    e.addChild(e2);
+
+    cout << e.toString();
+
     return 0;
 }
